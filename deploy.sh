542b32f637d4c940b624cc614e2fab95388fd76e
Habeeb Richard <rh745@zoo.cs.yale.edu>
2021-09-01 18:21:19 -0400

    minor update to deploy

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,7 @@ chmod u+rwx /c/cs323/proj4/submit
 
 echo "Copying results"
 cp -v bazel-bin/results_view/results-1 /c/cs323/proj1/results
+cp -v bazel-bin/results_view/results-4 /c/cs323/proj4/results
 chmod g+rws /c/cs323/proj1/results
 chmod u+rwx /c/cs323/proj1/results
 chmod g+rws /c/cs323/proj4/results
